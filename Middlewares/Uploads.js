5c07e79103be59b2ff82ed079a089520d04bc3e9
rajendra-chimala <rajendrachimala5@gmail.com>
2025-03-22 09:17:32 +0545

    Vercel File Commit

--- Middlewares/Uploads.js
+++ Middlewares/Uploads.js
@@ -1,17 +1,15 @@
 const multer = require("multer");
 const path = require("path");
 
-// Set Storage Engine
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "uploads/"); // Folder to store images
+        cb(null, "uploads/");
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
     }
 });
 
-// File Filter (Allow only images)
 const fileFilter = (req, file, cb) => {
     const allowedFileTypes = /jpeg|jpg|png/;
     const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
@@ -24,10 +22,10 @@ const fileFilter = (req, file, cb) => {
     }
 };
 
-// Upload Middleware
+
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 2 * 1024 * 1024 }, // Limit file size to 2MB
+    limits: { fileSize: 2 * 1024 * 1024 },
     fileFilter: fileFilter
 });
 
